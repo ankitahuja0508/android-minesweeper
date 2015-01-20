54442b8f1c4580c9ddf9724c330137effd16de3d
Sebastian Ng <sebng85@gmail.com>
2015-01-20 01:07:38 -0800

    [MS-1] Implemented interface of main view with app logo and 2 buttons: "New Game" and "Continue".

---
+++ app/src/main/java/com/sebng/minesweeper/activity/MainActivity.java
@@ -0,0 +1,45 @@
+package com.sebng.minesweeper.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.sebng.minesweeper.R;
+import com.sebng.minesweeper.fragment.MainFragment;
+
+
+public class MainActivity extends Activity
+        implements MainFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, MainFragment.newInstance())
+                .commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
